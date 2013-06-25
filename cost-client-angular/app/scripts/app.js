68ef057f57c3aa5eb736dc3cc9ed0d4ebc40669f
fischermatte@gmx.net <fischermatte@gmx.net>
2013-06-26 01:56:39 +0200

    added Cost $resource

--- cost-client-angular/app/scripts/app.js
+++ cost-client-angular/app/scripts/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('costsApp', ['ngGrid','ngResource']).
+angular.module('costsApp', ['ngGrid', 'ngResource']).
   config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {templateUrl: 'views/cost-list.html', controller: 'CostListCtrl'});
     $routeProvider.when('/costs', {templateUrl: 'views/cost-list.html', controller: 'CostListCtrl'});
     $routeProvider.when('/costs/new', {templateUrl: 'views/cost-form.html', controller: 'CostNewCtrl'});
     $routeProvider.when('/about', {templateUrl: 'views/about.html'});
+  }]).
+  factory('Costs', ['$resource', function ($resource) {
+    return $resource('https://cost-server.appspot.com/costs/:costId', { costId: '@id' })
   }]);
 
 
