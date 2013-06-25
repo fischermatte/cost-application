eadbabf18e5406a889a17aa5ad48dd5fb7ef6fcd
fischermatte@gmx.net <fischermatte@gmx.net>
2013-06-25 23:44:01 +0200

    changed dates of costs for showing sorting

--- cost-server/src/main/java/com/unisys/ch/jax/costserver/service/CostServiceInMemory.java
+++ cost-server/src/main/java/com/unisys/ch/jax/costserver/service/CostServiceInMemory.java
@@ -13,21 +13,22 @@ import com.unisys.ch.jax.costserver.model.Cost;
 @Service
 public class CostServiceInMemory implements CostService {
 
-    private Long DUMMY_COUNT = 5L;
+    private Long NUMBER_OF_ENTRIES_TO_GENERATE = 100L;
+    private static final Calendar INITIAL_DATE = Calendar.getInstance();
+    {
+        INITIAL_DATE.set(Calendar.YEAR, 2013);
+        INITIAL_DATE.set(Calendar.MONTH, 0);
+        INITIAL_DATE.set(Calendar.DAY_OF_MONTH, 1);
+        INITIAL_DATE.set(Calendar.HOUR, 18);
+        INITIAL_DATE.set(Calendar.MINUTE, 00);
+    }
+
     private TreeMap<Long, Cost> cachedToDos;
 
     public CostServiceInMemory() {
         cachedToDos = new TreeMap<Long, Cost>();
         generateDummyEntities();
     }
-
-    private void generateDummyEntities() {
-        for (long x = 1; x <= DUMMY_COUNT; x++) {
-            Cost todo = new Cost(x, "Aufwand " + x, "Aufwand " + x + " Beschreibung, z.B. JIRA-7329", Calendar.getInstance(), 8.0, Project.BAFU);
-            cachedToDos.put(x, todo);
-        }
-    }
-
     public Cost findById(Long id) {
         return cachedToDos.get(id);
     }
@@ -38,7 +39,7 @@ public class CostServiceInMemory implements CostService {
 
     public Cost save(Cost todo) {
         if (todo.getId() == null) {
-            todo.setId(++DUMMY_COUNT);
+            todo.setId(++NUMBER_OF_ENTRIES_TO_GENERATE);
         }
         return cachedToDos.put(todo.getId(), todo);
     }
@@ -57,4 +58,19 @@ public class CostServiceInMemory implements CostService {
                     Math.min(start + amount, retVal.size()));
         }
     }
+
+    private void generateDummyEntities() {
+        for (long x = 1; x <= NUMBER_OF_ENTRIES_TO_GENERATE; x++) {
+            Cost todo = new Cost(x, "Aufwand " + x, "Siehe JIRA-" + x, createDate(x), 8.0, Project.BAFU);
+            cachedToDos.put(x, todo);
+        }
+    }
+
+    private Calendar createDate(long x) {
+        Calendar date = Calendar.getInstance();
+        date.setTime(INITIAL_DATE.getTime());
+        date.add(Calendar.DAY_OF_YEAR, (int) x);
+        return date;
+    }
+
 }
