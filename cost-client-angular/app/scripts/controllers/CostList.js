a662d06d74c6f50e385c3200ec2702784d2ae545
fischermatte@gmx.net <fischermatte@gmx.net>
2013-06-26 01:13:13 +0200

    simplified CostListCtrl

--- cost-client-angular/app/scripts/controllers/CostList.js
+++ cost-client-angular/app/scripts/controllers/CostList.js
@@ -2,10 +2,6 @@
 
 angular.module('costsApp')
   .controller('CostListCtrl', function ($scope, $http) {
-    $scope.filterOptions = {
-      filterText: '',
-      useExternalFilter: true
-    };
     $scope.pagingOptions = {
       pageSizes: [20, 50, 100],
       pageSize: 20,
@@ -20,34 +16,20 @@ angular.module('costsApp')
         $scope.$apply();
       }
     };
-    $scope.getPagedDataAsync = function (pageSize, page, searchText) {
+    $scope.getPagedDataAsync = function (pageSize, page) {
       setTimeout(function () {
         var data;
         var url = 'https://cost-server.appspot.com/costs';
-//        var url = 'http://localhost:8080/costs';
-        if (searchText) {
-          var ft = searchText.toLowerCase();
-          $http.get(url).success(function (largeLoad) {
-            data = largeLoad.filter(function (item) {
-              return JSON.stringify(item).toLowerCase().indexOf(ft) !== -1;
-            });
-            $scope.setPagingData(data, page, pageSize);
-          });
-        } else {
-          $http.get(url).success(function (largeLoad) {
-            $scope.setPagingData(largeLoad, page, pageSize);
-          });
-        }
+        // var url = 'http://localhost:8080/costs';
+        $http.get(url).success(function (largeLoad) {
+          $scope.setPagingData(largeLoad, page, pageSize);
+        });
       }, 100);
     };
-
     $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
 
     $scope.$watch('pagingOptions', function () {
-      $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-    }, true);
-    $scope.$watch('filterOptions', function () {
-      $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+      $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
     }, true);
 
     $scope.gridOptions = {
@@ -55,11 +37,12 @@ angular.module('costsApp')
       enablePaging: true,
       showFooter: true,
       pagingOptions: $scope.pagingOptions,
-      filterOptions: $scope.filterOptions,
-      columnDefs: [ {field:'id', displayName:'ID'},
-        {field:'title', displayName:'Titel'},
-        {field:'time', displayName:'Verbuchte Zeit'},
-        {field:'project', displayName:'Projekt'},
-        {field:'workDay', displayName:'Datum', cellFilter:'date:"dd.MM.yyyy - HH:mm"'}]
+      columnDefs: [
+        {field: 'id', displayName: 'ID'},
+        {field: 'title', displayName: 'Titel'},
+        {field: 'time', displayName: 'Verbuchte Zeit'},
+        {field: 'project', displayName: 'Projekt'},
+        {field: 'workDay', displayName: 'Datum', cellFilter: 'date:"dd.MM.yyyy - HH:mm"'}
+      ]
     };
   });
