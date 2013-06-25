cc5241aa08509655891c9b8c1a786a8f5a20535c
fischermatte@gmx.net <fischermatte@gmx.net>
2013-06-25 22:47:52 +0200

    added about page

--- cost-client-angular/app/scripts/app.js
+++ cost-client-angular/app/scripts/app.js
@@ -6,6 +6,7 @@ angular.module('costsApp', ['ngGrid']).
     $routeProvider.when('/', {templateUrl: 'views/cost-list.html', controller: 'CostListCtrl'});
     $routeProvider.when('/costs', {templateUrl: 'views/cost-list.html', controller: 'CostListCtrl'});
     $routeProvider.when('/costs/new', {templateUrl: 'views/cost-form.html', controller: 'CostNewCtrl'});
+    $routeProvider.when('/about', {templateUrl: 'views/about.html'});
 
     //$locationProvider.html5Mode(true);
 
