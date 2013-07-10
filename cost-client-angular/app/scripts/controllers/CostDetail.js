e11a30cb11e2bdf50a8e56de37b2ae56f70eec1a
fischermatte <fischermatte@gmx.net>
2013-07-10 23:12:39 +0200

    added delete functionality

--- cost-client-angular/app/scripts/controllers/CostDetail.js
+++ cost-client-angular/app/scripts/controllers/CostDetail.js
@@ -3,7 +3,7 @@
 angular.module('costsApp')
   .controller('CostDetailCtrl', function ($scope, $routeParams, $location, CostService) {
     // Project selection list
-    $scope.projects = ['BAFU','BIT','EJPD','UNISYS','POST','VBS'];
+    $scope.projects = ['BAFU', 'BIT', 'EJPD', 'UNISYS', 'POST', 'VBS'];
 
     if ($routeParams.costId === 'new') {
       // NEW
@@ -12,14 +12,20 @@ angular.module('costsApp')
     } else {
       // LOAD
       $scope.title = 'Aufwand bearbeiten';
-      CostService.get({id: $routeParams.costId}, function(cost){
+      CostService.get({id: $routeParams.costId}, function (cost) {
         cost.workDay = new Date(cost.workDay);
         $scope.cost = cost;
       });
     }
 
-    $scope.save = function (){
-      CostService.save($scope.cost,function(){
+    $scope.save = function () {
+      CostService.save($scope.cost, function () {
+        $location.url('/costs');
+      });
+    };
+
+    $scope.delete = function () {
+      CostService.delete({id: $scope.cost.id}, function () {
         $location.url('/costs');
       });
     };
